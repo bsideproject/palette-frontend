639617732c7a489c7e2543dab7ad2e01c15987a0
geunyongkim <rmsdyd20@gmail.com>
2022-07-16 13:27:13 +0900

    사용하지 않는 Page 삭제, Setting
    Login Page 외 삭제, SignUp Component, Image Picker 삭제

--- Front_End/src/screens/Signin.js
+++ Front_End/src/screens/Signin.js
@@ -1,12 +1,10 @@
-import React, {useContext, useState, useRef, useEffect} from 'react';
-import {ThemeContext} from 'styled-components/native';
+import React, {useContext, useState} from 'react';
 import styled from 'styled-components/native';
-import {Button, Image, Input, ErrorMessage} from '../components';
+import {Button, Image, ErrorMessage} from '../components';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
-import {signup, signin} from '../db_connect';
+import {signin} from '../db_connect';
 import {Alert} from 'react-native';
-import {validateEmail, removeWhiteSpace} from '../utils';
 import {UserContext, ProgressContext} from '../contexts';
 
 const Container = styled.View`
@@ -24,38 +22,18 @@ const LOGO =
 
 const Signin = ({navigation}) => {
   const insets = useSafeAreaInsets();
-  const theme = useContext(ThemeContext);
   const {setUser} = useContext(UserContext);
   const {spinner} = useContext(ProgressContext);
-
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
-  const [disabled, setDisabled] = useState(true);
-  const refPassword = useRef(null);
-
-  useEffect(() => {
-    setDisabled(!(email && password && !errorMessage));
-  }, [email, password, errorMessage]);
-
-  const _handleEmailChange = email => {
-    const changedEmail = removeWhiteSpace(email);
-    setEmail(changedEmail);
-    setErrorMessage(
-      validateEmail(changedEmail) ? '' : 'Please Verify your Email',
-    );
-  };
-  const _handlePasswordChange = password => {
-    setPassword(removeWhiteSpace(password));
-  };
 
   const _handleSigninBtnPress = async () => {
     try {
       spinner.start();
-      const user = await signin({email, password});
+      const user = await signin({});
       setUser(user);
     } catch (e) {
       Alert.alert('Signin Error', e.message);
+      setErrorMessage('Fail..');
     } finally {
       spinner.stop();
     }
@@ -68,36 +46,8 @@ const Signin = ({navigation}) => {
       contentContainerStyle={{flex: 1}}>
       <Container insets={insets}>
         <Image url={LOGO} />
-        <Input
-          label="Email"
-          placeholder="Email"
-          returnKeyType="next"
-          value={email}
-          onChangeText={_handleEmailChange}
-          onSubmitEditing={() => refPassword.current.focus()}
-        />
-        <Input
-          ref={refPassword}
-          label="Password"
-          placeholder="password"
-          returnKeyType="done"
-          value={password}
-          onChangeText={_handlePasswordChange}
-          isPassword={true}
-          onSubmitEditing={_handleSigninBtnPress}
-        />
         <ErrorMessage message={errorMessage} />
-        <Button
-          title="Sign in"
-          onPress={_handleSigninBtnPress}
-          disabled={disabled}
-        />
-        <Button
-          title="or sign up"
-          onPress={() => navigation.navigate('Signup')}
-          containerStyle={{marginTop: 0, backgroundColor: 'transparent'}}
-          textStyle={{color: theme.btnTextLink, fontSize: 18}}
-        />
+        <Button title="Sign in" onPress={_handleSigninBtnPress} />
       </Container>
     </KeyboardAwareScrollView>
   );
